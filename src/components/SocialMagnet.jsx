0093c76e414b7968dda107762815894e3112c504
Shivam Kumar <shivamlpu1214@gmail.com>
2026-03-21 18:33:49 +0530

    SocialMagnet added

--- src/components/SocialMagnet.jsx
+++ src/components/SocialMagnet.jsx
@@ -20,19 +20,15 @@ const ICONS = {
   GitHub: (props) => (
     <svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M12,2.2467A10.00042,10.00042,0,0,0,8.83752,21.73419c.5.08752.6875-.21247.6875-.475,0-.23749-.01251-1.025-.01251-1.86249C7,19.85919,6.35,18.78423,6.15,18.22173A3.636,3.636,0,0,0,5.125,16.8092c-.35-.1875-.85-.65-.01251-.66248A2.00117,2.00117,0,0,1,6.65,17.17169a2.13742,2.13742,0,0,0,2.91248.825A2.10376,2.10376,0,0,1,10.2,16.65923c-2.225-.25-4.55-1.11254-4.55-4.9375a3.89187,3.89187,0,0,1,1.025-2.6875,3.59373,3.59373,0,0,1,.1-2.65s.83747-.26251,2.75,1.025a9.42747,9.42747,0,0,1,5,0c1.91248-1.3,2.75-1.025,2.75-1.025a3.59323,3.59323,0,0,1,.1,2.65,3.869,3.869,0,0,1,1.025,2.6875c0,3.83747-2.33752,4.6875-4.5625,4.9375a2.36814,2.36814,0,0,1,.675,1.85c0,1.33752-.01251,2.41248-.01251,2.75,0,.26251.1875.575.6875.475A10.0053,10.0053,0,0,0,12,2.2467Z"/></svg>
   ),
-  Twitter: (props) => (
-    <svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"/></svg>
-  ),
   Mail: (props) => (
     <svg fill="currentColor" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" {...props}><title>mail</title><path d="M64 128Q64 113 73 105 81 96 96 96L416 96Q431 96 440 105 448 113 448 128L448 144 256 272 64 144 64 128ZM256 328L448 200 448 384Q448 416 416 416L96 416Q64 416 64 384L64 200 256 328Z" /></svg>
   ),
 };
 
 const SOCIALS = [
-  { name: 'LinkedIn', url: 'https://www.linkedin.com/in/ashif-elahi-1740302b3', icon: ICONS.LinkedIn },
-  { name: 'GitHub', url: 'https://github.com/Deadcoder001', icon: ICONS.GitHub },
-  { name: 'X', url: 'https://x.com/Ashif_Elahi01', icon: ICONS.Twitter },
-  { name: 'Mail', url: 'mailto:asifelahi6@gmail.com', icon: ICONS.Mail },
+  { name: 'LinkedIn', url: 'https://www.linkedin.com/in/shivam-kumar1214/', icon: ICONS.LinkedIn },
+  { name: 'GitHub', url: 'https://github.com/shivam121214', icon: ICONS.GitHub },
+  { name: 'Mail', url: 'mailto:shivamlpu1214@gmail.com', icon: ICONS.Mail },
 ];
 
 export default function SocialMagnet() {
